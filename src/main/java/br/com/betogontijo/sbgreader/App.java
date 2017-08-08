37067e8f2a5db4a231fee34de77aed677afecd13
BETO <BETO@192.168.25.6>
2017-08-08 17:48:46 -0300

    Initial commit

---
+++ src/main/java/br/com/betogontijo/sbgreader/App.java
@@ -0,0 +1,13 @@
+package br.com.betogontijo.sbgreader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
